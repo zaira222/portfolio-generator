03e70864a1a1535756a38c7eb05bcd2d03967a56
ZP <zairaperez01@gmail.com>
2022-01-24 15:01:32 -0600

    generate entire website

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
+const {writeFile, copyFile } = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template.js');
 
 //const pageHTML = generatePage(name, github);
@@ -142,12 +142,19 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-    
-    fs.writeFile('./index.html', pageHTML, err => {
-      // if (err) throw new Error(err);
-
-      //console.log('Page created! Checkout index.html in this directory to see it!');
-      // });
-    });
+    return generatePage(portfolioData);
   })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+   
