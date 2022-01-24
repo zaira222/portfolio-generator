33d9e788cc72c336b31d7106c0e60c9362fb4aa1
ZP <zairaperez01@gmail.com>
2022-01-24 13:08:28 -0600

    modified the HTML template to handle more data

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-//const fs = require('fs');
-//const generatePage = require('./src/page-template');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 //const pageHTML = generatePage(name, github);
 
@@ -142,5 +142,12 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
-  });
+    const pageHTML = generatePage(portfolioData);
+    
+    fs.writeFile('./index.html', pageHTML, err => {
+      // if (err) throw new Error(err);
+
+      //console.log('Page created! Checkout index.html in this directory to see it!');
+      // });
+    });
+  })
